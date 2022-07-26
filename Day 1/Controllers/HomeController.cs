e8ad82bb7a4dcdadd90509fce9f50b1e231c4bd5
sameermd11 <89958787+sameermd11@users.noreply.github.com>
2022-07-26 16:57:57 +0530

    Add files via upload

---
+++ Day 1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Employees()
+        {
+            return View(); 
+        }
+
+        public IActionResult Department()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
