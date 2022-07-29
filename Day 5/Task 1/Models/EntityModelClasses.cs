8a439dc1486be1420e1ca634707eebfac11d99ce
sameermd11 <89958787+sameermd11@users.noreply.github.com>
2022-07-29 15:23:55 +0530

    Add files via upload

---
+++ Day 5/Task 1/Models/EntityModelClasses.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication6.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpNo { get; set; }
+        public string Ename { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+        public int DeptNo { get; set; }
+    }
+
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> employees { get; set; }
+
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+            : base(options) { }
+    }
+}
